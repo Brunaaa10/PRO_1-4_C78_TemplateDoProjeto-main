777ecddd9a06bc2b3c01a0b2e940ec3ba89de0a0
brunaaa10 <brunaleticiadiniz1@gmail.com>
2023-09-25 15:11:05 -0300

    CICLISTA ELETRÔNICO - ESTÁGIO 4

---
+++ config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgrzAIym9PZHnnlN07undChSqVIsTJiyE",
+  authDomain: "ciclista-eletronico-13140.firebaseapp.com",
+  projectId: "ciclista-eletronico-13140",
+  storageBucket: "ciclista-eletronico-13140.appspot.com",
+  messagingSenderId: "268326245151",
+  appId: "1:268326245151:web:b19992b6c09905d6ff162a"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
